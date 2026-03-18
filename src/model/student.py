2c1679e303badf545f750dd59c10b4e0ddd42ba4
Veng-devit <vengdevit8@gmail.com>
2026-03-18 13:19:04 +0700

    update student, transcript report

--- src/model/student.py
+++ src/model/student.py
@@ -55,7 +55,9 @@ class Student(Person):
     @property
     def scores(self): 
         return self._scores
-
+    @scores.setter
+    def scores(self, new_score):
+        self._scores = new_score
     def add_score(self, subject, score):
         try:
             if subject not in self._scores.keys():
